609e667b0fbdecc49c1170f746b8623b4fa1667d
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 11:59:35 +0300

    Update lab7_1.c

--- lab7/lab7_1/lab7_1.c
+++ lab7/lab7_1/lab7_1.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE *fp;
+
+
+    fp = popen("rwho", "r");
+    if (fp == NULL) {
+        perror("popen failed");
+        return 1;
+    }
+
+
+    FILE *more = popen("more", "w");
+    if (more == NULL) {
+        perror("popen more failed");
+        pclose(fp);
+        return 1;
+    }
+
+
+    char buffer[1024];
+    while (fgets(buffer, sizeof(buffer), fp) != NULL) {
+        fputs(buffer, more);
+    }
+
+
+    pclose(fp);
+    pclose(more);
+
+    return 0;
+}
 

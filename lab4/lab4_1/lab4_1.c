5e7814ea3588767d51099726b2d86ff0a96a6aa4
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 10:27:18 +0300

    Update lab4_1.c

--- lab4/lab4_1/lab4_1.c
+++ lab4/lab4_1/lab4_1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+int main() {
+    size_t size = (size_t)1 << 63; 
+    void* ptr = malloc(size);
+
+    if (ptr == NULL) {
+        perror("malloc failed");
+    } else {
+        printf("malloc succeeded\n");
+        free(ptr);
+    }
+
+    return 0;
+}
 

b7ec7ffd1df8dfabddc71e25b5a1e42ecd5a984c
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 10:47:31 +0300

    Update lab4_4.c

--- lab4/lab4_4/lab4_4.c
+++ lab4/lab4_4/lab4_4.c
@@ -1,1 +1,21 @@
 
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    void *ptr = NULL;
+    for (int i = 0; i < 3; i++) {
+        ptr = malloc(100);
+        if (!ptr) {
+            perror("malloc failed");
+            exit(1);
+        }
+
+        printf("Iteration %d: ptr = %p\n", i, ptr);
+
+        free(ptr);   
+        ptr = NULL;  
+    }
+    return 0;
+}
+

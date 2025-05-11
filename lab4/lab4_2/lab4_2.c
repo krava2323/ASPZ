43aaabdf035e99653fda9cc346655f246c74b178
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 10:30:37 +0300

    Update lab4_2.c

--- lab4/lab4_2/lab4_2.c
+++ lab4/lab4_2/lab4_2.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main() {
+    int xa = 100000;
+    int xb = 30000;
+    int num = xa * xb;  
+
+    printf("xa = %d, xb = %d\n", xa, xb);
+    printf("num (as int) = %d\n", num);
+    printf("num (as size_t) = %zu\n", (size_t)num);
+
+    void* ptr = malloc(num);
+    if (ptr == NULL) {
+        perror("malloc failed");
+    } else {
+        printf("malloc succeeded\n");
+        free(ptr);
+    }
+
+    return 0;
+}

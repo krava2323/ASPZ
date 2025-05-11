279b2add01907ec7a2fb3aacbba37910c3f94e15
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 10:02:44 +0300

    Update lab3_7.c

--- lab3/lab3_7/lab3_7.c
+++ lab3/lab3_7/lab3_7.c
@@ -1,1 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <errno.h>
+
+int main() {
+    int count = 0;
+    int fd;
+    char filename[256];
+
+    while (1) {
+        snprintf(filename, sizeof(filename), "tmpfile_%d.txt", count);
+        fd = open(filename, O_CREAT | O_WRONLY, 0644);
+        if (fd == -1) {
+            perror("open");
+            printf("Total files opened: %d\n", count);
+            break;
+        }
+        count++;
+    }
+
+    return 0;
+}
 

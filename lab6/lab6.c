d85dc0d16d48d874dd5cb3517843cd6e2549a0d9
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 11:32:50 +0300

    Update lab6.c

--- lab6/lab6.c
+++ lab6/lab6.c
@@ -1,1 +1,21 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int *ptr = (int *)malloc(sizeof(int));  
+    if (ptr == NULL) {
+        perror("malloc failed");
+        return 1;
+    }
+
+    *ptr = 42;  
+    printf("Before free: %d\n", *ptr);
+
+    free(ptr);  
+
+    *ptr = 99;  
+    printf("After free: %d\n", *ptr); 
+
+    return 0;
+}

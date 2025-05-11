efb12f5191b8c8ba35afc0415772f34795ed72be
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 11:00:28 +0300

    Update lab4_6.c

--- lab4/lab4_6/lab4_6.c
+++ lab4/lab4_6/lab4_6.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    void *ptr1 = realloc(NULL, 100);
+    if (ptr1) {
+        printf("realloc(NULL, 100) succeeded: %p\n", ptr1);
+        free(ptr1);
+    } else {
+        printf("realloc(NULL, 100) failed\n");
+    }
+
+
+    void *ptr2 = malloc(50);
+    if (!ptr2) {
+        perror("malloc failed");
+        return 1;
+    }
+
+    void *ptr3 = realloc(ptr2, 0); 
+    printf("realloc(ptr, 0) returned: %p\n", ptr3);
+  
+
+    return 0;
+}
 

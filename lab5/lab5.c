fa2dfd16ae7372517b1a0566eab3a0826a60d234
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 11:25:29 +0300

    Update lab5.c

--- lab5/lab5.c
+++ lab5/lab5.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+
+void recurse(int depth) {
+    char buffer[1024];  
+    buffer[0] = 1;      
+    printf("Recursion depth: %d\n", depth);
+    if (depth < 10000) {
+        recurse(depth + 1);  
+    }
+}
+
+int main() {
+    recurse(1);
+    return 0;
+}
 

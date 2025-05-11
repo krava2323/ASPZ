0c737ca41618c7b79ab2c73c4d526a3dfd02f497
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 16:09:16 +0300

    Update lab7_3.c

--- lab7/lab7_3/lab7_3.c
+++ lab7/lab7_3/lab7_3.c
@@ -1,1 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LINE 1024
+
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, "Usage: %s <word> <file>\n", argv[0]);
+        return 1;
+    }
+
+    char *word = argv[1];
+    char *filename = argv[2];
+    char line[MAX_LINE];
+
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Failed to open file");
+        return 1;
+    }
+
+    while (fgets(line, MAX_LINE, file)) {
+        if (strstr(line, word)) {
+            printf("%s", line);
+        }
+    }
+
+    fclose(file);
+    return 0;
+}
 

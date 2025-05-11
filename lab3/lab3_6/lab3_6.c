7be93f1569a763f21448244fa6b8f1f43d1ba930
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 09:57:31 +0300

    Update lab3_6.c

--- lab3/lab3_6/lab3_6.c
+++ lab3/lab3_6/lab3_6.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void recurse(int depth) {
+    char buffer[1024]; 
+    printf("Recursion depth: %d\n", depth);
+    buffer[0] = depth % 256; 
+    recurse(depth + 1);    
+}
+
+int main() {
+    recurse(1);
+    return 0;
+}
 

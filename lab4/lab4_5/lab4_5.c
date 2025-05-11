8e050015b1c9e608c6ab8a4a5188a227f31adb51
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 10:50:55 +0300

    Update lab4_5.c

--- lab4/lab4_5/lab4_5.c
+++ lab4/lab4_5/lab4_5.c
@@ -1,1 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int main() {
+    size_t big_size = (size_t)1024 * 1024 * 1024 * 8; 
+    void *ptr = malloc(1024);
+    if (!ptr) {
+        perror("initial malloc failed");
+        return 1;
+    }
+
+    printf("Initial malloc succeeded: %p\n", ptr);
+
+    void *tmp = realloc(ptr, big_size);  
+    if (!tmp) {
+        perror("realloc failed");
+
+        free(ptr);
+    } else {
+        printf("Realloc succeeded: %p\n", tmp);
+        free(tmp);
+    }
+
+    return 0;
+}
 

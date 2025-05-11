241b9c87483fd176c33bd4ba15513b00ac877369
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-11 11:15:35 +0300

    Update lab4_7.c

--- lab4/lab4_7/lab4_7.c
+++ lab4/lab4_7/lab4_7.c
@@ -1,6 +1,32 @@
+#define _GNU_SOURCE
+#include <stdio.h>
 #include <stdlib.h>
 
-struct sbar *ptr, *newptr;
-ptr = calloc(1000, sizeof(struct sbar));
-newptr = reallocarray(ptr, 500, sizeof(struct sbar));
+struct sbar {
+    int a;
+    double b;
+};
 
+int main() {
+    struct sbar *ptr, *newptr;
+
+    ptr = calloc(1000, sizeof(struct sbar));
+    if (!ptr) {
+        perror("calloc failed");
+        return 1;
+    }
+
+    newptr = reallocarray(ptr, 500, sizeof(struct sbar));
+    if (!newptr) {
+        perror("reallocarray failed");
+        free(ptr);  
+        return 1;
+    }
+
+    ptr = newptr;
+
+
+
+    free(ptr);
+    return 0;
+}

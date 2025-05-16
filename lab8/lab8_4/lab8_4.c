3a397973ad3ea6429960136418f93f58f7013e28
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-16 15:04:23 +0300

    Update lab8_4.c

--- lab8/lab8_4/lab8_4.c
+++ lab8/lab8_4/lab8_4.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+#include <unistd.h>
+
+int main() {
+    int pid;
+    pid = fork();
+
+    if (pid == -1) {
+
+        perror("fork");
+        return 1;
+    }
+
+    printf("pid = %d (from process %d)\n", pid, getpid());
+    return 0;
+}
 

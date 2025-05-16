aa538e569eb96b78be3d46d9d40eb8cb9279ff80
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-16 16:21:36 +0300

    Update lab9_1.c

--- lab9/lab9_1.c
+++ lab9/lab9_1.c
@@ -1,1 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pwd.h>
+
+#define UID_THRESHOLD 1000  
+
+int main() {
+    FILE *fp;
+    char line[512];
+    int found_other_users = 0;
+
+    uid_t my_uid = getuid(); 
+    struct passwd *pw = getpwuid(my_uid);
+    const char *my_username = pw ? pw->pw_name : "unknown";
+
+    fp = popen("getent passwd", "r");
+    if (fp == NULL) {
+        perror("popen");
+        return 1;
+    }
+
+    printf("Your UID: %d, Username: %s\n", my_uid, my_username);
+    printf("Search other users (UID >= %d)...\n\n", UID_THRESHOLD);
+
+    while (fgets(line, sizeof(line), fp)) {
+        char *username = strtok(line, ":");
+        strtok(NULL, ":"); 
+        char *uid_str = strtok(NULL, ":");
+        if (!username || !uid_str) continue;
+
+        int uid = atoi(uid_str);
+
+        if (uid >= UID_THRESHOLD && strcmp(username, my_username) != 0) {
+            found_other_users = 1;
+            printf("Found user: %s (UID: %d)\n", username, uid);
+        }
+    }
+
+    pclose(fp);
+
+    if (!found_other_users) {
+        printf("\n no otherusers found.\n");
+    }
+
+    return 0;
+}
 

78ff86023c5d4b51c1d0ccdbfa2f6b065ef87524
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-16 17:02:15 +0300

    Update lab9_4.c

--- lab9/lab9_4/lab9_4.c
+++ lab9/lab9_4/lab9_4.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf("User \n");
+    int ret1 = system("whoami");
+
+    if (ret1 == -1) {
+        perror("Error");
+        return 1;
+    }
+
+    printf("\n Info \n");
+    int ret2 = system("id");
+
+    if (ret2 == -1) {
+        perror("Error id");
+        return 1;
+    }
+
+    return 0;
+}
 

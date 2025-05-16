72a65b5f04aa58a3916d739f7fadfe76ca9503f2
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-16 14:42:58 +0300

    Update lab8_1.c

--- lab8/lab8_1/lab8_1.c
+++ lab8/lab8_1/lab8_1.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+
+int main() {
+    int pipefd[2];
+    char buffer[4096]; 
+    memset(buffer, 'X', sizeof(buffer));
+
+
+    if (pipe(pipefd) == -1) {
+        perror("pipe");
+        return 1;
+    }
+
+
+    int flags = fcntl(pipefd[1], F_GETFL);
+    fcntl(pipefd[1], F_SETFL, flags | O_NONBLOCK);
+
+
+    ssize_t count = write(pipefd[1], buffer, sizeof(buffer));
+
+    if (count == -1) {
+        perror("write");
+    } else if (count < sizeof(buffer)) {
+        printf( erite  %zd bytes with %zu\n", count, sizeof(buffer));
+    } else {
+        printf(" write %zd bytes\n", count);
+    }
+
+    close(pipefd[0]);
+    close(pipefd[1]);
+
+    return 0;
+}
 

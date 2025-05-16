7aaf993a2c855e3b7a1943067e2f155bf976bffd
krava2323 <kravchykaleksandr400@gmail.com>
2025-05-16 16:30:03 +0300

    Update lab9_2.c

--- lab9/lab9_2/lab9_2.c
+++ lab9/lab9_2/lab9_2.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    printf(" read /etc/shadow \n");
+
+    int ret = system("sudo cat /etc/shadow");
+
+    if (ret != 0) {
+        fprintf(stderr, "Failed read /etc/shadow. \n");
+        return 1;
+    }
+
+    return 0;
+}
 
